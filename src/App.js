261e07e0e80f176b37207572d02bbfa26d623983
Mike <migueljrojas@gmail.com>
2020-10-18 13:31:43 -0300

    Sets communication between components and validation flow

--- src/App.js
+++ src/App.js
@@ -3,12 +3,29 @@ import Form from './components/Form'
 import Message from './components/Message'
 
 class App extends Component {
+    constructor() {
+      super();
+
+      this.state = {
+        validationMessage: ''
+      }
+
+      this.setValidationMessage = this.setValidationMessage.bind(this);
+    }
+
+    setValidationMessage(message) {
+      this.setState({validationMessage: message});
+    }
 
     render() {
-        return (<div>
-            <Form></Form>
-            <Message></Message>
-        </div>);
+        const { validationMessage } = this.state;
+        
+        return (
+          <div>
+            <Form onFormValidation={this.setValidationMessage} />
+            <Message message={validationMessage} />
+          </div>
+        );
     }
 }
 
--- src/components/Form.js
+++ src/components/Form.js
@@ -11,24 +11,71 @@ class Form extends Component {
             isUrlValid: false,
         };
 
+        this.validateForm = this.validateForm.bind(this);
     }
+
+    validateForm() {
+      const { onFormValidation: sendMessage } = this.props;
+      const isFormInvalidMessage = 'Form is Incomplete!';
+      const isFormValidMessage = 'Form is Complete!';
+      if (
+        this.isValidName() &&
+        this.isValidEmail() &&
+        this.isValidPhone() &&
+        this.isValidUrl()
+      ) {
+        sendMessage(isFormValidMessage)
+      } else {
+        sendMessage(isFormInvalidMessage)
+      }
+    }
+
+    isValidName() {
+      return false;
+    }
+
+    isValidEmail() {
+      return false;
+    }
+
+    isValidPhone() {
+      return false;
+    }
+
+    isValidUrl() {
+      return false;
+    }    
     
     render() {
         return (
             <div className="row">
             <h1 className="text-center">Form Validation</h1>
             <form>
-                <h3>Name:
-                </h3>
-                <h3>Email:
-                </h3>
-                <h3>Phone:
-                </h3>
-                <h3>Blog URL:
-                </h3>
-                <div className="small-6 small-centered text-center columns">
-                    <a href="#" className="button success expand round text-center">Verify</a>
-                </div>
+              <div className="form-group">
+                <label>Name:</label>
+                <input type="text" />
+              </div>
+              <div className="form-group">
+                <label>Email:</label>
+                <input type="email" />
+              </div>
+              <div className="form-group">
+                <label>Phone:</label>
+                <input type="tel" />
+              </div>
+              <div className="form-group">
+                <label>Blog URL:</label>
+                <input type="url" />
+              </div>
+              <div className="small-6 small-centered text-center columns">
+                  <a
+                    href="#"
+                    className="button success expand round text-center"
+                    onClick={this.validateForm}
+                  >
+                    Verify
+                  </a>
+              </div>
             </form>
         </div>);
     }
